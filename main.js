6481a089e6305a29125afbaf57ef1b1e39e52dc5
Sofia-Ortega <s.ortega@tamu.edu>
2023-04-10 00:56:36 -0700

    halfStep function tested

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 
 const NOTES_SHARPS = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 const NOTES_FLATS = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+const TOTAL_NOTES_COUNT = 12;
 
-function increaseHalfStep(note, flatMode=false) {
+// makes sure that index is in range
+function boundIndex(index) {
+    if (index >= TOTAL_NOTES_COUNT) {
+        return index % TOTAL_NOTES_COUNT;
+    } else if(index < 0) {
+        return TOTAL_NOTES_COUNT + index;
+    }
 
-    const NOTES = NOTES_SHARPS;
-    if(note.includes("b")) notes = NOTES_FLATS;
+    return index;
+    
+}
+
+// note: string, increase: bool, flatMode: bool
+function halfStep(note, increase, flatMode=false) {
 
     // parse note
     var base = note[0];
@@ -17,18 +28,39 @@ function increaseHalfStep(note, flatMode=false) {
 
     if(flat || flatMode) {
         // flat case
-        var str = base + flat ? "b" : "" 
-        var index = NOTES_FLATS.findIndex(str) % NOTES_FLATS.length;
-        newNote = NOTES_FLATS[index+1];
+        var str = base + (flat ? "b" : "");
+        var index = NOTES_FLATS.indexOf(str);
+
+        if(index == -1) throw new Error("in half step, note not found")
+        
+        increase ? index++ : index--;
+        index = boundIndex(index);
+
+        newNote = NOTES_FLATS[index];
     } else {
         // sharp case
         var str = base + (sharp ? "#" : "");
-        var index = NOTES_SHARPS.findIndex(str) % NOTES_SHARPS.length;
-        newNote = NOTES_SHARPS[index + 1]
+        var index = NOTES_SHARPS.indexOf(str);
+
+        if(index == -1) throw new Error("in half step, note not found")
+
+        increase ? index++ : index--;
+        index = boundIndex(index);
+
+        newNote = NOTES_SHARPS[index];
     }
 
     return newNote;
 
+}
+
+
+
+var INCREASING = true;
+var FLAT_MODE = true;
+
+for(var i = 0; i < NOTES_SHARPS.length; i++) {
+    console.log(halfStep(NOTES_FLATS[i], !INCREASING, FLAT_MODE));
 }
 
 
