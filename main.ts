bc408fbb4499931591057337f3020829fff33016
Sofia-Ortega <s.ortega@tamu.edu>
2023-04-10 08:05:28 -0700

    finished adding types to main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 
-const NOTES_SHARPS = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-const NOTES_FLATS = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-const TOTAL_NOTES_COUNT = 12;
+const NOTES_SHARPS  : string[] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+const NOTES_FLATS : string [] = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+const TOTAL_NOTES_COUNT : number = 12;
 
-const SHARP_MODE = false;
-const FLAT_MODE = true;
+const SHARP_MODE : boolean = false;
+const FLAT_MODE : boolean = true;
 
 // makes sure that index is in range
-function boundIndex(index) {
+function boundIndex(index : number) : number {
     if (index >= TOTAL_NOTES_COUNT) {
         return index % TOTAL_NOTES_COUNT;
     } else if(index < 0) {
@@ -19,22 +19,22 @@ function boundIndex(index) {
 }
 
 // note: string, increase: bool, flatMode: bool
-function halfStep(note : string, increase : boolean, flatMode? : boolean)  : string{
+function halfStep(note : string, increase : boolean, flatMode? : boolean)  : string {
     
     if(flatMode == undefined) flatMode = false;
 
     // parse note
-    var base = note[0];
-    var sharp = note.includes("#");
-    var flat = note.includes("b");
+    var base : string = note[0];
+    var sharp : boolean = note.includes("#");
+    var flat : boolean = note.includes("b");
 
     // find new note
-    var newNote = "x"; 
+    var newNote : string = "x"; 
 
     if(flat || flatMode) {
         // flat case
-        var str = base + (flat ? "b" : "");
-        var index = NOTES_FLATS.indexOf(str);
+        var str : string = base + (flat ? "b" : "");
+        var index : number = NOTES_FLATS.indexOf(str);
 
         if(index == -1) throw new Error("in half step, note not found")
         
@@ -68,13 +68,14 @@ function decreaseHalfStep(note : string) : string {
     return halfStep(note, false, SHARP_MODE)
 }
 
-var INCREASING = true;
+
+
+// -------------- TESTING -------------------
+var INCREASING : boolean = true;
 
 for(var i = 0; i < NOTES_SHARPS.length; i++) {
     console.log(increaseHalfStep(NOTES_SHARPS[i]))
 }
-
-console.log("---------------")
 
 for(var i = 0; i < NOTES_SHARPS.length; i++) {
     console.log(decreaseHalfStep(NOTES_SHARPS[i]))
